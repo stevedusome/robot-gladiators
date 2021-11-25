78fbaf68295f6e2fd57888af2ec4c1b8a8983aa8
Steve Dusome <stevedusome@gmail.com>
2021-11-25 10:27:02 -0500

    finished mvp

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,9 +72,53 @@ var fight = function(enemyName) {
   };
 
 
-// run fight function to start game
-for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+// function to start a new game
+var startGame = function() {
+      // reset players stats
+
+      playerHealth = 100;
+      playerAttack = 10;
+      playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+  endGame();
+};
+
+// function to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+// checks for second playthrough before ending game
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 }
+}
+
+startGame();
